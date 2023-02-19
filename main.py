114d0670e873255ccd25f796060d6925159255bd
HakiCodes <nokolie4@gmail.com>
2023-02-19 11:51:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import speedtest as st
+
+#Set best server
+server = st.Speedtest()
+server.get_best_server()
+
+#Test Download Speed
+download = server.download()
+download /= 1000000
+print(f"Download Speed: {download} Mb/s")
+
+#Test Upload Speed
+
+upload = server.upload()
+upload /= 1000000
+print(f"Upload Speed: {upload} Mb/s")
+
+#Test Ping
+ping = server.results.ping
+print(f"Ping: {ping}")
