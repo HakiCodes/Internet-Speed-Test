23d2131645c6fdb52444c405ea793f843a69cfba
HakiCodes <109916516+HakiCodes@users.noreply.github.com>
2023-02-24 11:07:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ server = st.Speedtest()
 server.get_best_server()
 
 #Test Download Speed
+print("Testing......")
 download = server.download()
 download /= 1000000
 print(f"Download Speed: {download} Mb/s")
 
 #Test Upload Speed
-
 upload = server.upload()
 upload /= 1000000
 print(f"Upload Speed: {upload} Mb/s")
